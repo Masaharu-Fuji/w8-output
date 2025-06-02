0bbdc9e6949a0074910df48550c514a60bb7b861
Masaharu-Fuji <pencil68872@gmail.com>
2025-06-03 02:56:46 +0900

    modify js/d233

--- paiza/level-d/233.js
+++ paiza/level-d/233.js
@@ -1,16 +1,16 @@
 process.stdin.resume();
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding("utf8");
 // 自分の得意な言語で
 // Let's チャレンジ！！
 var lines = [];
-var reader = require('readline').createInterface({
+var reader = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
-reader.on('line', (line) => {
+reader.on("line", (line) => {
   lines.push(line);
 });
-reader.on('close', () => {
+reader.on("close", () => {
   const sum = lines.reduce((a, b) => parseInt(a) + parseInt(b));
   console.log(sum);
 });
